b3980f3eb847e189064db969330011b79abbc8d5
jpdevz <jpa.pizarro@gmail.com>
2025-09-28 20:52:26 -0300

    RED: test /ping debe responder 200 OK (aún no implementado) + .gitignore

---
+++ src/main/java/com/ejemplo/usuarios_app/UsuariosAppApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.usuarios_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejemplo/usuarios_app/UsuariosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.usuarios_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
